b564af21e1816733425552c16577898e73427f4e
Marina <Marina@10.0.111.100>
2015-11-20 19:26:33 +0100

    Marina Barea Medina

---
+++ src/barea/Barea.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package barea;
+
+/**
+ *
+ * @author Marina
+ */
+public class Barea {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Marina Barea Medina
+    }
+    
+}
